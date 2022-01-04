e9d9361b3466c9357686a020923ae74cb71aa34b
DamingWang <370729147@qq.com>
2022-01-04 11:38:49 +0800

    集成Eureka注册中心

---
+++ eureka-server/src/main/java/cn/qhy/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package cn.qhy.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author qhy
+ * @date 2022/1/4 10:26
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
--- goods-service/src/main/java/cn/qhy/goods/GoodsApplication.java
+++ goods-service/src/main/java/cn/qhy/goods/GoodsApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author qhy
@@ -12,6 +13,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication(scanBasePackages = "cn.qhy.*")
 @MapperScan("cn.qhy.**.mapper")
 @ServletComponentScan("cn.qhy.*")
+@EnableEurekaClient
 public class GoodsApplication {
 
     public static void main(String[] args) {
